f1f0f2045418582168f39832fe7b2820cd5d226e
GQSM <ms314006@hotmail.com>
2019-08-07 22:27:21 +0800

    [ADD] 增加圖曾向左移動的刷新

--- src/lib/HorizonLine.ts
+++ src/lib/HorizonLine.ts
@@ -3,6 +3,10 @@ import { IHorizonLine } from './interface/IHorizonLine';
 class HorizonLine implements IHorizonLine {
   canvas: HTMLCanvasElement;
 
+  FPS: number = 0;
+
+  image: string;
+
   sourceXPos: any[] = [];
 
   xPos: any[] = [];
@@ -16,14 +20,44 @@ class HorizonLine implements IHorizonLine {
 
   dimensions: any = {
     width: 600,
-    height: 12,
-    YPos: 127,
+    height: 300,
+    YPos: 0,
   };
 
-  constructor(canvas: HTMLCanvasElement, spritePos: { x: number, y: number, }) {
+  constructor(
+    canvas: HTMLCanvasElement,
+    spritePos: { x: number, y: number, },
+    image: string,
+    FPS: number
+  ) {
     this.canvas = canvas;
     this.spritePos = spritePos;
+    this.image = image;
+    this.FPS = FPS;
     this.init();
+    this.draw();
+  }
+
+  updateXPos = (pos: number, incre: number): void => {
+    const line1 = pos;
+    const line2 = pos === 0 ? 1 : 0;
+    this.xPos[line1] -= incre;
+    this.xPos[line2] = this.xPos[line1] + this.dimensions.width;
+
+    if (this.xPos[line1] <= -this.dimensions.width) {
+      this.xPos[line1] += this.dimensions.width * 2;
+      this.xPos[line2] = this.xPos[line1] - this.dimensions.width;
+      this.sourceXPos[line1] = this.spritePos.x;
+    }
+  }
+
+  update = (deltaTime: number, speed: number) => {
+    const incre = Math.floor(speed * (this.FPS / 1000) * deltaTime);
+    if (this.xPos[0] <= 0) {
+      this.updateXPos(0, incre);
+    } else {
+      this.updateXPos(1, incre);
+    }
     this.draw();
   }
 
@@ -42,17 +76,16 @@ class HorizonLine implements IHorizonLine {
 
   draw = (): void => {
     const ctx: any = this.canvas.getContext('2d');
-
     ctx.drawImage(
-      document.getElementById('offline-resources-1x'),
+      document.getElementById(this.image),
       this.sourceXPos[0], this.spritePos.y,
       this.dimensions.width, this.dimensions.height,
       this.xPos[0], this.yPos,
       this.dimensions.width, this.dimensions.height
     );
     ctx.drawImage(
-      document.getElementById('offline-resources-1x'),
-      this.sourceXPos[1], this.spritePos.y,
+      document.getElementById(this.image),
+      this.sourceXPos[0], this.spritePos.y,
       this.dimensions.width, this.dimensions.height,
       this.xPos[1], this.yPos,
       this.dimensions.width, this.dimensions.height
